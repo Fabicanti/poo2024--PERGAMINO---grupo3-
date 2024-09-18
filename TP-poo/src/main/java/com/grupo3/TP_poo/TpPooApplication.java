0ebd16ce4a24b30c807a2f5390044879b4355888
fabian <cantisanofabi@gmail.com>
2024-09-18 17:16:34 -0300

    primer commit

---
+++ TP-poo/src/main/java/com/grupo3/TP_poo/TpPooApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.TP_poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpPooApplication.class, args);
+	}
+
+}
