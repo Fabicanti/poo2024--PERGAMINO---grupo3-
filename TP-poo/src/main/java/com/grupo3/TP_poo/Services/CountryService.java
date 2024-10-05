010cadbbd412195cead1d5940496dcbb420df679
fabian <cantisanofabi@gmail.com>
2024-10-05 11:34:11 -0300

    completo service, controller

--- TP-poo/src/main/java/com/grupo3/TP_poo/Services/CountryService.java
+++ TP-poo/src/main/java/com/grupo3/TP_poo/Services/CountryService.java
@@ -1,5 +1,7 @@
 package com.grupo3.TP_poo.Services;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.grupo3.TP_poo.Entitis.Country;
@@ -11,6 +13,15 @@ public class CountryService {
     @Autowired
     private CountryRepository countryRepository;
 
+    public List<Country> retriveAllCountries(){
+        return countryRepository.findAll();
+    }
+
+    public Country getCountry(Long id){
+        return countryRepository.findById(id)
+                        .orElseThrow(()-> new NoSuchElementException());
+    }
+
     public Country addCountry(Country country){
         Country newCountry = new Country();
         newCountry.setId(country.getId());
@@ -21,6 +32,7 @@ public class CountryService {
     }
 
     public boolean modifyCountry(Country country){
+
         return true;
     }
 
--- TP-poo/src/main/java/com/grupo3/TP_poo/TpPooApplication.java
+++ TP-poo/src/main/java/com/grupo3/TP_poo/TpPooApplication.java
@@ -3,6 +3,8 @@ package com.grupo3.TP_poo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//liquid base, manejo de versionado de bd
+
 @SpringBootApplication
 public class TpPooApplication {
 
