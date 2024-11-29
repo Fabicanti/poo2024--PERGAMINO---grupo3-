9ba1811098de76b572d4e0734c87863dd7bf24d5
fabian <cantisanofabi@gmail.com>
2024-11-29 00:53:13 -0300

    funcionando eureka y gateway

---
+++ eurekaServer/src/test/java/ar/edu/unnoba/poo2024/eureka_server/Eureka_serverApplicationTests.java
@@ -0,0 +1,15 @@
+package ar.edu.unnoba.poo2024.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+@EnableEurekaServer
+class Eureka_serverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
